52d55821ddb38f5e17114dbc5ee3ad361f457a16
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-24 16:26:06 +0200

    2) Components: structure page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Header from './components/Header';
-import './App.css';
 import Logo from './assets/Logo.svg'
+import Header from './components/Header';
+import Profile from './pages/Profile'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header logo={Logo} />
+      <Profile userName={"GuiFraV"}/>
     </div>
   );
 }
