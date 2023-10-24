2cddc09f84d069c6c8d8e544174c2ce5429ae459
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-24 19:31:37 +0200

    3) Components: add Projects and ProjectsDetail using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import Logo from './assets/Logo.svg'
 import Header from './components/Header';
-import Profile from './pages/Profile'
+import Profile from './pages/Profile';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Projects from './pages/Projects';
+import ProjectDetails from './pages/ProjectDetails';
 
 function App() {
   return (
     <div className="App">
-      <Header logo={Logo} />
-      <Profile userName={"GuiFraV"}/>
+      <BrowserRouter>
+        <Header logo={Logo} />
+        <Routes>
+          <Route 
+            path='/'
+            element={<Profile userName="GuiFraV" />}
+          />
+          <Route 
+            path='/projects'
+            element={<Projects userName="GuiFraV" />}
+          />
+          <Route 
+            path='/projects/:name'
+            element={<ProjectDetails userName="GuiFraV" />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
